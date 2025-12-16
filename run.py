453b030ca91c2d3d921a27ba2cdede9b4489a5ad
namiii2205 <200380761+namiii2205@users.noreply.github.com>
2025-12-16 16:26:15 +0700

    commit

--- run.py
+++ run.py
@@ -5,18 +5,20 @@ import json
 import random
 import pandas as pd
 import asyncio
+from tqdm.asyncio import tqdm_asyncio
 
 agent = init_orchestrator()
 format_agent = init_format_agent()
-number_of_answers = 2
+
+number_of_answers = -1
 concurrent = 5
+question_path = "data/question_samples/test.json"
 
 semaphore = asyncio.Semaphore(concurrent)
 
 class MultipleChoiceQuestion(BaseModel):
     question: str
     choices: List[str]
-
 
 async def process_one_question(question: dict):
     async with semaphore:
@@ -41,23 +43,25 @@ async def process_one_question(question: dict):
 
         return answer
 
-
 async def main():
-    with open("data/question_samples/val.json", mode="r", encoding="utf-8") as f:
+    with open(question_path, mode="r", encoding="utf-8") as f:
         questions = json.load(f)
-        if number_of_answers >= 0:
-            questions = random.sample(questions, number_of_answers)
+
+    if number_of_answers >= 0:
+        questions = random.sample(questions, number_of_answers)
 
     tasks = [
         process_one_question(question)
         for question in questions
     ]
 
-    answers = await asyncio.gather(*tasks)
+    answers = await tqdm_asyncio.gather(
+        *tasks,
+        desc=f"Processing questions (concurrent={concurrent})"
+    )
 
     answer_df = pd.DataFrame(answers)
     answer_df.to_csv("submission.csv", index=False)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
